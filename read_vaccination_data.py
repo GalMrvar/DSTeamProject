2c0c865da778e21bdf7caa2070fc1b476cf5fd1b
betulsezer <e1633057@student.tuwien.ac.at>
2021-10-15 17:39:13 +0200

    created dataframe for vaccinations data with calculation of the percentage

--- read_vaccination_data.py
+++ read_vaccination_data.py
@@ -1,31 +1,35 @@
 import pandas as pd
+from sqlalchemy import create_engine
 
-filepath = "data/covid/owid-covid-data.csv"
+filepath = "app/data/covid/owid-covid-data.csv"
 
 rows_to_keep = ['AFG']
 vaccination_data = pd.read_csv(filepath,
                                sep=",",
                                header=0,
                                parse_dates= ['date'],
-                               usecols=['iso_code','location','date','total_cases','total_vaccinations','people_vaccinated','people_fully_vaccinated','total_boosters','new_vaccinations','new_vaccinations_smoothed','total_vaccinations_per_hundred','people_vaccinated_per_hundred','people_fully_vaccinated_per_hundred','total_boosters_per_hundred','new_vaccinations_smoothed_per_million'])
+                               usecols=['iso_code','location','date','total_cases','total_vaccinations','people_vaccinated','people_fully_vaccinated','total_boosters','new_vaccinations','new_vaccinations_smoothed','total_vaccinations_per_hundred','people_vaccinated_per_hundred','people_fully_vaccinated_per_hundred','total_boosters_per_hundred','new_vaccinations_smoothed_per_million', 'population'])
 #filter by country
-filtered = vaccination_data[(vaccination_data['iso_code'].isin(['USA','DEU','CHE','ISR']))]
+filtered = vaccination_data[(vaccination_data['iso_code'].isin(['DEU','CHE','ISR']))]
 
 #filter by start of vaccination in countries
-## USA vaccination data has lots of missing values in columns "total_vaccinations" and "people_vaccinated" but no missing values 
-## in column "new_vaccinations_smoothed"
-usa_vaccination_data = filtered[(filtered['date']>'2020-12-19') & (filtered['iso_code'].isin(['USA']))]
 deu_vaccination_data = filtered[(filtered['date']>'2020-12-26') & (filtered['iso_code'].isin(['DEU']))]
 che_vaccination_data = filtered[(filtered['date']>'2020-12-21') & (filtered['iso_code'].isin(['CHE']))]
 isr_vaccination_data = filtered[(filtered['date']>'2020-12-18') & (filtered['iso_code'].isin(['ISR']))]
 
-#filling missing values with mean
-usa_vaccination_data['people_fully_vaccinated'].fillna((usa_vaccination_data['people_fully_vaccinated'].mean()), inplace=True)
-deu_vaccination_data['people_fully_vaccinated'].fillna((deu_vaccination_data['people_fully_vaccinated'].mean()), inplace=True)
-che_vaccination_data['people_fully_vaccinated'].fillna((che_vaccination_data['people_fully_vaccinated'].mean()), inplace=True)
-isr_vaccination_data['people_fully_vaccinated'].fillna((isr_vaccination_data['people_fully_vaccinated'].mean()), inplace=True)
+#calculate the percentage of vaccinated people by country
+isr_vaccination_data['people_fully_vaccinated_in_percentage'] = isr_vaccination_data['people_fully_vaccinated']/isr_vaccination_data['population']
+deu_vaccination_data['people_fully_vaccinated_in_percentage'] = deu_vaccination_data['people_fully_vaccinated']/deu_vaccination_data['population']
+che_vaccination_data['people_fully_vaccinated_in_percentage'] = che_vaccination_data['people_fully_vaccinated']/che_vaccination_data['population']
 
-print(usa_vaccination_data.isnull().sum())
-print(deu_vaccination_data.isnull().sum())
-print(che_vaccination_data.isnull().sum())
-print(isr_vaccination_data.isnull().sum())
+#merge the processed data into one dataframe
+vaccination_data_processed = [che_vaccination_data, deu_vaccination_data, isr_vaccination_data]
+print(type(vaccination_data_processed))
+
+db_conn = create_engine("postgresql://username:secret@db:5432/database")
+
+df = pd.DataFrame(vaccination_data_processed)
+df.to_sql('vaccinations', db_conn, if_exists='replace')
+df2 = pd.read_sql_query('SELECT people_fully_vaccinated_in_percentage FROM vaccinations',db_conn)
+print(df2)
+#print(vaccination_data_processed)
