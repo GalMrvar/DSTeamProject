407efa205b6b546a706baa26aa2100fb426b0d45
François Macaire <francois.macaire@gmail.com>
2021-10-15 15:01:55 +0200

    added code for displying a dataframe queried from db

--- app/app.py
+++ app/app.py
@@ -12,22 +12,16 @@ from dash import dash_table
 
 app = dash.Dash(__name__)
 
-
-#COPIED HERE CODE FROM READ_VACCINATION_DATA FOR DEBUGGING PURPOSE
-
-####################################################################
-
-
 db_conn = create_engine("postgresql://username:secret@db:5432/database")
 
 app.logger.info(db_conn.connect())
 df = pd.DataFrame(np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]), columns=['a', 'b', 'c'])
-#df.to_sql('test', db_conn, if_exists='replace')
-#df2 = pd.read_sql_query('select * from "test" WHERE "a" = 1',db_conn)
+df.to_sql('test', db_conn, if_exists='replace')
+df2 = pd.read_sql_query('SELECT a FROM test',db_conn)
 
 app.layout = dash_table.DataTable(
     id='table',
-    columns=[{"name": i, "id": i} for i in df.columns],
+    columns=[{"name": i, "id": i} for i in df2.columns],
     data=df.to_dict('records'),
 )
 
