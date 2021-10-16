7df50deb2ff6cc61ba8a425f792184a6fcb1381a
langchristian <92402901+langchristian@users.noreply.github.com>
2021-10-16 16:07:48 +0200

    reading aviation Data plus putting it into DF

--- app/read_aviation_data.py
+++ app/read_aviation_data.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sqlalchemy import create_engine
+
 #2020
 filepath20 = "app/data/flights/2020-States-Flights.csv"
 
@@ -8,7 +9,7 @@ flight_data_2020 = pd.read_csv(filepath20,
                                header=0,
                                parse_dates= ['Day'],
                                usecols=['Entity','Week','Day','Flights','Flights (7-day moving average)','Day 2019','Flights 2019 (Reference)','% vs 2019 (Daily)','% vs 2019 (7-day Moving Average)'])
-#filter by counttry
+#filter by country
 filtered = flight_data_2020[(flight_data_2020['Entity'].isin(['Germany','Switzerland','Israel']))]
 
 #filter by start of aviation in countries
@@ -25,7 +26,7 @@ flight_data_2021 = pd.read_csv(filepath21,
                                header=0,
                                parse_dates= ['Day'],
                                usecols=['Entity','Week','Day','Flights','Flights (7-day moving average)','Day 2019','Flights 2019 (Reference)','% vs 2019 (Daily)','% vs 2019 (7-day Moving Average)', 'Day Previous Year', 'Flights Previous Year'])
-#filter by counttry
+#filter by country
 filtered21 = flight_data_2021[(flight_data_2021['Entity'].isin(['Germany','Switzerland','Israel']))]
 
 #filter by start of aviation in countries
@@ -36,3 +37,9 @@ isr_flights21_data = filtered21[(filtered21['Day']>='2021-01-01') & (filtered21[
 #merge the processed data into one dataframe
 flights_data_processed = [che_flights20_data, deu_flights20_data, isr_flights20_data, che_flights21_data, deu_flights21_data, isr_flights21_data,]
 print(flights_data_processed)
+
+db_conn = create_engine("postgresql://username:secret@db:5432/database")
+
+df_aviation = pd.DataFrame(flights_data_processed)
+df_aviation.to_sql('aviation', db_conn, if_exists='replace')
+print(df)
